921fdfde598fa37c6d31c6bdbb1a6046af27b107
jeremyDudo <jmdudo2@gmail.com>
2019-10-05 17:18:32 -0400

    main.rs added

--- rusty_mirror/src/main.rs
+++ rusty_mirror/src/main.rs
@@ -1,3 +1,14 @@
+mod lib;
+use lib::*;
+
 fn main() {
-    println!("Hello, world!");
+    const NN1: f32 = 1.46;
+    const NN2: f32 = 4.6;
+    const DN1: f32 = 52.75;
+    const DN2: f32 = 52.75;
+    const DSUB: f32 = 1000.0;
+    const TOTLAY: u32 = 101;
+
+    quad_show(&NN1, &NN2, &DN1, &DN2, &DSUB, &TOTLAY);
+
 }
